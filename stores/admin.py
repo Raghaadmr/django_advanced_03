9c8398285acdfdf17c8dcf3c21e6386b8775545c
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-09-13 10:20:30 +0300

    done

--- stores/admin.py
+++ stores/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Store
 
-# Register your models here.
-admin.site.register(Store)
+
+
+class StoreAdmin(admin.ModelAdmin):
+	Display = ('id','name','description')
+	Search = ['name','description']
+	Add_filter = ['added']
+	Clickable =( 'id','name')
+	Editable=['description']
+admin.site.register(Store,StoreAdmin)
